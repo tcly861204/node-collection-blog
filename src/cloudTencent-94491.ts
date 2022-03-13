793b37a1fea99b7756d3612946768f4ae3892ead
tcly861204 <tcly861204@hotmail.com>
2022-03-13 20:06:45 +0800

    feat: 优化排版

---
+++ src/cloudTencent-94491.ts
@@ -0,0 +1,38 @@
+import request, { RequestCallback } from 'request'
+import cheerio from 'cheerio'
+const uri = 'https://cloud.tencent.com/developer/column/94491'
+const cloudTencent = function () {
+  return new Promise((reslove, reject) => {
+    const callback: RequestCallback = function (error, response, body) {
+      if (error) {
+        reject(error)
+      } else {
+        const $ = cheerio.load(body, {
+          decodeEntities: false
+        })
+        const group = $('body').find('.com-article-list').find('.com-article-panel')
+        const data: Array<string> = []
+        group.each(function(index) {
+          let title = $(this).find('.com-article-panel-title').html()
+          title = title!.replace(/(?<=(href="))/, 'https://cloud.tencent.com')
+            .replace('hotrep=""', `
+              style="
+                color: #999;
+                font-weight: normal;
+                text-decoration: none;
+                font-size: 16px;
+              "
+            `)
+          data.push(`\t<li style="list-style-type: none; margin: 0 0 15px 0; padding: 0;">
+            <span style="color: #999; font-size: 16px;">${index+1}. </span>
+            ${title}
+          </li>`)
+        })
+        reslove(data)
+      }
+    }
+    return request.get(uri, callback)
+  })
+}
+
+export default cloudTencent
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import render from "./render"
 import sendMail from "./email"
 import cloudTencent from './cloudTencent'
 import cnode from './cnode'
+import cloudTencent94491 from './cloudTencent-94491'
+import cloudTencentVue from './cloudTencent-vue'
 (async() => {
   const cloudTencentData = await cloudTencent()
   const cnodeData = await cnode()
+  const cloudTencent94491Data = await cloudTencent94491()
+  const cloudTencentVueData = await cloudTencentVue()
   const Style = `
     font-size: 20px; background: #ff0000; color: #fff; padding: 2px 6px; border-radius: 3px; margin: 0;
   `
@@ -15,6 +19,14 @@ import cnode from './cnode'
       <span style="${Style}">腾讯云</span>
     </li>`,
     ...(cloudTencentData as Array<string>),
+    `<li style="list-style-type: none; margin: 0 0 15px 0;">
+      <span style="${Style}">Vue中文社区</span>
+    </li>`,
+    ...(cloudTencentVueData as Array<string>),
+    `<li style="list-style-type: none; margin: 0 0 15px 0;">
+      <span style="${Style}">南山种子外卖跑手的专栏</span>
+    </li>`,
+    ...(cloudTencent94491Data as Array<string>),
     `<li style="list-style-type: none; margin: 0 0 15px 0;">
       <span style="${Style}">cnode</span>
     </li>`,
