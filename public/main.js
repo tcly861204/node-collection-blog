caedb1939483f83d20831608c1272e9b628bc544
tcly861204 <tcly861204@hotmail.com>
2022-04-01 14:45:03 +0000

    Deploying to gh-pages from  @ 795b34a629a2b4b60835238dd8cf4b6c8b22cf74 🚀

--- public/main.js
+++ public/main.js
@@ -1,10 +1,19 @@
 window.onload = function () {
   const progress = document.body.querySelector('.progress')
-  const screenHeight = window.innerHeight
-  const screenWidth = window.innerWidth
-  const bodyHeight = document.body.offsetHeight - screenHeight
-  window.addEventListener('scroll', function (e) {
+  let screenHeight = window.innerHeight
+  let screenWidth = window.innerWidth
+  let bodyHeight = document.body.offsetHeight - screenHeight
+  const handleScroll = function (e) {
     // @ts-ignore
     progress.style.width = Math.floor((e.currentTarget.scrollY / bodyHeight) * screenWidth) + 'px'
+  }
+  window.addEventListener('resize', function (e) {
+    screenHeight = window.innerHeight
+    screenWidth = window.innerWidth
+    bodyHeight = document.body.offsetHeight - screenHeight
+    handleScroll(e)
+  })
+  window.addEventListener('scroll', function (e) {
+    handleScroll(e)
   }, false)
 }
